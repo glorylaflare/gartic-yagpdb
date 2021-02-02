56f9c12383eb9e709b6ecfa139265d952c11bfe7
glorylaflare <juninho_toh@hotmail.com>
2021-02-02 15:07:46 -0300

    main

--- eventos/EE frames.go
+++ eventos/EE frames.go
@@ -1,4 +1,4 @@
-{{ $Channel := 780191143962869780 }}
+{{ $channel := 780191143962869780 }}
 {{ $args := parseArgs 1 "" (carg "string" "value" )}}
 {{ $out := ($args.Get 0) "512" }}
 {{ $Rembed := cembed
@@ -7,7 +7,7 @@
 "image" (sdict "url" $out)
 "footer"  (sdict "text" (joinStr "" "Você terá 25 segundos para descobrir!"))
 }}
-{{ $mID := sendMessageRetID $Channel (complexMessage "embed" $Rembed )}}
+{{ $mID := sendMessageRetID $channel (complexMessage "embed" $Rembed )}}
 {{ deleteTrigger 0 }}
 {{ sleep 25 }} 
 {{ editMessage $channel $mID (complexMessageEdit "embed" nil "content" "O tempo acabou pessoal...Aguardem o resultado!") }}
